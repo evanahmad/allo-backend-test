27fe604fde727182607dcf0a895827c682a09075
evanahm <ahmad.fansyuri@bni.co.id>
2026-01-11 10:19:04 +0700

    [DEV-001] add springboot application entry point & test

---
+++ finance-aggregator-service/src/test/java/id/co/evan/project/aggregator/FinanceAggregatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.evan.project.aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceAggregatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
