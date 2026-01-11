f1a586b1796e88eeb598b53f10b5fa31f9ab2051
evanahm <ahmad.fansyuri@bni.co.id>
2026-01-11 16:58:53 +0700

    [DEV-001] adjust unit test

--- finance-aggregator-service/src/test/java/id/co/evan/project/aggregator/service/strategy/impl/LatestIdrStrategyTest.java
+++ finance-aggregator-service/src/test/java/id/co/evan/project/aggregator/service/strategy/impl/LatestIdrStrategyTest.java
@@ -41,28 +41,23 @@ class LatestIdrStrategyTest {
 
     @BeforeEach
     void setUp() {
-        // Inject value @Value dari application.yml secara manual untuk test
         ReflectionTestUtils.setField(strategy, "githubUsername", "evanahmad");
     }
 
     @Test
     void shouldFetchAndTransformDataCorrectly() {
-        // 1. Mock Data Response dari Frankfurter
         ExternalRateResponse mockResponse = new ExternalRateResponse(
             1.0, "IDR", "2026-01-11", Map.of("USD", 0.000063)
         );
 
-        // 2. Mock Fluent API WebClient (Mocking Chain)
         when(webClient.get()).thenReturn(requestHeadersUriSpec);
         when(requestHeadersUriSpec.uri(anyString())).thenReturn(requestHeadersSpec);
         when(requestHeadersSpec.retrieve()).thenReturn(responseSpec);
         when(responseSpec.bodyToMono(ExternalRateResponse.class)).thenReturn(Mono.just(mockResponse));
 
-        // 3. Mock Util
         when(spreadFactorUtil.calculateSpreadFactor("evanahmad")).thenReturn(0.00933);
         when(spreadFactorUtil.calculateBuySpread(0.000063, 0.00933)).thenReturn(16021.11);
 
-        // 4. Execution & Verification menggunakan StepVerifier
         StepVerifier.create(strategy.fetchData())
             .assertNext(response -> {
                 assert response.resourceType().equals("latest_idr_rates");
